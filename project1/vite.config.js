a63a5994b14b460235084b3518db26d9a02689c7
Faiz Malik <mrfaizmalik609@gmail.com>
2025-08-23 01:32:18 +0500

    Fixed path issue for Vercel build

--- project1/vite.config.js
+++ project1/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./", // ⚡ relative paths ka support (important for Vercel)
+  base: "./", // yeh relative paths ke liye zaroori hai
 });
