7e62f4b1f2501d5746a2250a46e73ce197af3ffc
Faiz Malik <mrfaizmalik609@gmail.com>
2025-08-23 01:25:57 +0500

    ok

--- project1/src/main.jsx
+++ project1/src/main.jsx
@@ -1,9 +1,12 @@
+// src/main.jsx
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")).render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
