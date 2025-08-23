ea786cc0ab9fb779564a6dd68e835e2fe9e931a4
Faiz Malik <mrfaizmalik609@gmail.com>
2025-08-23 22:12:25 +0500

    ok

--- project1/src/main.jsx
+++ project1/src/main.jsx
@@ -1,8 +1,9 @@
+// src/main.jsx  (ya index.jsx)
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { CartProvider } from "./context/CartContext.jsx"; // ✅ correct path
+import { CartProvider } from "./context/CartContext.jsx"; // ✅ sahi import
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
